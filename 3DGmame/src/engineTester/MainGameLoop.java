4640aa06590858b573b462b15a5626afb271e246
ShiPeng <spone.2014@gmail.com>
2016-05-31 02:47:47 +0800

    3rd Person View

--- 3DGmame/src/engineTester/MainGameLoop.java
+++ 3DGmame/src/engineTester/MainGameLoop.java
@@ -10,8 +10,11 @@ import org.lwjgl.util.vector.Vector3f;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import entities.Player;
 import models.RawModel;
 import models.TexturedModel;
+import objConverter.ModelData;
+import objConverter.OBJFileLoader;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
@@ -20,6 +23,8 @@ import renderEngine.EntityRenderer;
 import shaders.StaticShader;
 import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 
 public class MainGameLoop {
 
@@ -30,26 +35,93 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		
-		RawModel model = OBJLoader.loadObjModel("tree", loader);
+		// ********Terrain Texture Stuff *********
+		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("dirt"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("pinkFlowers"));
+		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
 		
-		TexturedModel staticModel = new TexturedModel(model, 
-				new ModelTexture(loader.loadTexture("tree")));
+		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
+		// ***************************************
 		
-		TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader),
+		ModelData treeData = OBJFileLoader.loadOBJ("tree");
+		
+		RawModel treeModel = loader.loadToVAO(treeData.getVertices(), 
+				treeData.getTextureCoords(), 
+				treeData.getNormals(), 
+				treeData.getIndices());
+		
+		//TexturedModel staticModel = new TexturedModel(OBJLoader.loadObjModel("tree", loader), 
+		//		new ModelTexture(loader.loadTexture("tree")));
+		TexturedModel tree = new TexturedModel(treeModel, 
+						new ModelTexture(loader.loadTexture("tree")));
+		
+		ModelData grassData = OBJFileLoader.loadOBJ("grassModel");
+		RawModel grassModel = loader.loadToVAO(grassData.getVertices(), 
+				grassData.getTextureCoords(), 
+				grassData.getNormals(),
+				grassData.getIndices());
+		TexturedModel grass = new TexturedModel(grassModel,
 				new ModelTexture(loader.loadTexture("grassTexture")));
+		
+		//TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader),
+		//		new ModelTexture(loader.loadTexture("grassTexture")));
+		
 		grass.getTexture().setHasTransparency(true);
 		grass.getTexture().setUseFakeLighting(true);
-		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
-				new ModelTexture(loader.loadTexture("fern")));
+		
+		//TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
+		//		new ModelTexture(loader.loadTexture("fern")));
+		
+		
+		ModelData fernData = OBJFileLoader.loadOBJ("fern");
+		RawModel fernModel = loader.loadToVAO(fernData.getVertices(), 
+				fernData.getTextureCoords(), 
+				fernData.getNormals(),
+				fernData.getIndices());
+		TexturedModel fern = new TexturedModel(fernModel,
+				new ModelTexture(loader.loadTexture("fernTexture")));
 		fern.getTexture().setHasTransparency(true);
 		
 		//texture.setShineDamper(10);
 		//texture.setRelectivity(1);
 		
+		
+		ModelData lowPolyTreeData = OBJFileLoader.loadOBJ("lowPolyTree");
+		RawModel lowPolyTreeModel = loader.loadToVAO(lowPolyTreeData.getVertices(), 
+				lowPolyTreeData.getTextureCoords(), 
+				lowPolyTreeData.getNormals(),
+				lowPolyTreeData.getIndices());
+		TexturedModel lowPolyTree = new TexturedModel(lowPolyTreeModel,
+				new ModelTexture(loader.loadTexture("lowPolyTree")));
+		
+		
+		ModelData flowerData = OBJFileLoader.loadOBJ("grassModel");
+		RawModel flowerModel = loader.loadToVAO(flowerData.getVertices(), 
+				flowerData.getTextureCoords(), 
+				flowerData.getNormals(),
+				flowerData.getIndices());
+		TexturedModel flower = new TexturedModel(flowerModel,
+				new ModelTexture(loader.loadTexture("flower")));
+		flower.getTexture().setHasTransparency(true);
+		flower.getTexture().setUseFakeLighting(true);
+		
+		ModelData bunnyData = OBJFileLoader.loadOBJ("bunny");
+		RawModel bunnyModel = loader.loadToVAO(bunnyData.getVertices(), 
+				bunnyData.getTextureCoords(), 
+				bunnyData.getNormals(),
+				bunnyData.getIndices());
+		TexturedModel bunny = new TexturedModel(bunnyModel,
+				new ModelTexture(loader.loadTexture("white")));
+		
+		Player player = new Player(bunny, new Vector3f(100, 0 , -50), 0 ,0, 0, 1);
+		
+		
 		List<Entity> entities = new ArrayList<Entity>();
 		Random random = new Random();
 		for (int i = 0; i< 500; i++) {
-			entities.add(new Entity(staticModel,
+			entities.add(new Entity(tree,
 					new Vector3f(random.nextFloat() * 800 - 400, 0 , random.nextFloat() *- 600),
 					0, 0, 0, 3));
 			entities.add(new Entity(grass,
@@ -58,17 +130,23 @@ public class MainGameLoop {
 			entities.add(new Entity(fern, 
 					new Vector3f(random.nextFloat()* 800 - 400, 0, random.nextFloat() *- 600),
 					0, 0, 0, 0.6f));
+			entities.add(new Entity(lowPolyTree,
+					new Vector3f(random.nextFloat() * 800 - 400, 0 , random.nextFloat() *- 600),
+					0, 0, 0, 0.5f));
+			entities.add(new Entity(flower,
+					new Vector3f(random.nextFloat() * 800 - 400, 0 , random.nextFloat() *- 600),
+					0, 0, 0, 1f));
 		}
 		
 		
 		
 		
 		Light light = new Light(new Vector3f(3000, 2000, 2000), new Vector3f(1,1,1));
-		Camera camera =new Camera();
+		Camera camera =new Camera(player);
 		
 		
-		Terrain terrain = new Terrain(-1,-1,loader, new ModelTexture(loader.loadTexture("grass")));
-		Terrain terrain2 = new Terrain(0, -1,loader, new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain = new Terrain(-1,-1,loader, texturePack, blendMap);
+		Terrain terrain2 = new Terrain(0, -1,loader, texturePack, blendMap);
 		
 		
 		
@@ -76,6 +154,8 @@ public class MainGameLoop {
 		while(!Display.isCloseRequested()){
 			
 			camera.move();
+			player.move();
+			renderer.processEntity(player);
 			
 			for (Entity entity: entities) {
 				renderer.processEntity(entity);
--- 3DGmame/src/entities/Camera.java
+++ 3DGmame/src/entities/Camera.java
@@ -1,15 +1,21 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
+	
+	private float distanceFromPlayer = 50;
+	private float angleAroundPlayer = 0;
 	private Vector3f position = new Vector3f(10,5,100);
-	private float pitch = 10;
+	private float pitch;
 	private float yaw;
 	private float roll;
 	
-	public Camera() {
+	private Player player;
+	public Camera(Player player) {
+		this.player = player;
 		
 	}
 	
@@ -17,6 +23,7 @@ public class Camera {
 		//System.out.println(position.x);
 		//System.out.println(position.y);
 		//System.out.println(position.z);
+		/*
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			position.z -= 1f;
 		}
@@ -35,6 +42,28 @@ public class Camera {
 		if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
 			position.y += 1f;
 		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_Z)) {
+			this.pitch += 1f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_X)) {
+			this.pitch -= 1f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
+			this.yaw += 1f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_T)) {
+			this.yaw -= 1f;
+		}
+		*/
+		calculateZoom();
+		calculatePitch();
+		calculateAngleAroundPlayer();
+		
+		float horizontalDistance = calculateHorizontalDistance();
+		float verticalDistance = calculateVerticalDistance();
+		calculateCameraPosition(horizontalDistance, verticalDistance);
+		this.yaw = 180 - (player.getRotY() + angleAroundPlayer);
+		
 		
 	}
 
@@ -53,5 +82,41 @@ public class Camera {
 	public float getRoll() {
 		return roll;
 	}
+	
+	private void calculateCameraPosition(float horizDistance, float verticDistance) {
+		float theta = player.getRotY() + angleAroundPlayer;
+		float offsetX = (float) (horizDistance * Math.sin(Math.toRadians(theta)));
+		float offsetZ  = (float) (horizDistance * Math.cos(Math.toRadians(theta)));
+		position.x = player.getPosition().x - offsetX;
+		position.z = player.getPosition().z - offsetZ;
+		position.y = player.getPosition().y + verticDistance;
+	}
+	
+	private float calculateHorizontalDistance() {
+		return (float) (distanceFromPlayer * Math.cos(Math.toRadians(pitch)));
+	}
+	
+	private float calculateVerticalDistance() {
+		return (float) (distanceFromPlayer * Math.sin(Math.toRadians(pitch)));
+	}
 
+	
+	private void calculateZoom() {
+		float zoomLevel = Mouse.getDWheel() * 0.1f;
+		distanceFromPlayer -= zoomLevel;
+	}
+	
+	private void calculatePitch() {
+		if (Mouse.isButtonDown(1)) {
+			float pitchChange = Mouse.getDY() * 0.1f;
+			pitch -= pitchChange;
+		}
+	}
+	
+	private void calculateAngleAroundPlayer() {
+		if (Mouse.isButtonDown(0)) {
+			float angleChange = Mouse.getDX() * 0.3f;
+			angleAroundPlayer -= angleChange;
+		}
+	}
 }
